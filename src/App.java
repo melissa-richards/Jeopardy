e5496f695a228d655f2791e192f25764cd8bf26c
Zachary <zachary.zheng@student.tdsb.on.ca>
2024-05-06 10:20:21 -0400

    DONUT NO WORKY

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        
+        Column_1 C1 = new Column_1();
+        C1.Q1();
     }
 }
--- src/Column_1.java
+++ src/Column_1.java
@@ -6,15 +6,47 @@ import javax.swing.JOptionPane;
 public class Column_1 {
     public void Q1() throws Exception {
 
-        String question_1 = JOptionPane.showInputDialog("Type all in lowercase");
-        String question_11 = question_1.toLowerCase();
-        String answer1 = "A";
-        if (question_11 == answer1){
-            JOptionPane.showMessageDialog(null, "You are correct");
+        int choice = Integer.parseInt(JOptionPane.showInputDialog("Pick a quesiton 1, 2, or 3"));
+        if (choice == 1) {
+            String question_1 = JOptionPane.showInputDialog("Name a police officers favourite snack");
+            JOptionPane.showMessageDialog(null, question_1);
+            /*String question_11 = question_1.toLowerCase();*/
+            if (question_1 == "donut"){
+                JOptionPane.showMessageDialog(null, "You are correct");
+            }
+            else if (question_1 != "donut"){
+                JOptionPane.showMessageDialog(null, "You are incorrect");
+            }
+            else {
+                JOptionPane.showMessageDialog(null, question_1);
+            }
         }
-        else {
-        JOptionPane.showMessageDialog(null, "You are incorrect");
+        /*
+        else if (choice == 2) {
+            String question_2 = JOptionPane.showInputDialog("Name what Ms.Richards is Alergic to.");
+            String question_22 = question_2.toLowerCase();
+            if (question_22 == "oranges"){
+                JOptionPane.showMessageDialog(null, "You are correct");
+            }
+            else if (question_22 != "oranges"){
+            JOptionPane.showMessageDialog(null, "You are incorrect");
+            }
+            else {
+            }
         }
-
+        else if (choice == 3) {
+            String question_3 = JOptionPane.showInputDialog("Name the first coca-cola bottle color.");
+            String question_33 = question_3.toLowerCase();
+            String answer3 = "green";
+            if (question_33 == answer3){
+                JOptionPane.showMessageDialog(null, "You are correct");
+            }
+            else if (question_33 != answer3){
+            JOptionPane.showMessageDialog(null, "You are incorrect");
+            }
+            else {
+            }
+        }
+*/
     }
 }
