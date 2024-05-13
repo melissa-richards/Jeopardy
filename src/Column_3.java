d59d329d2310450cc31a9189cdce1859070cbf1d
Eljas Muhonen <eljas.muhonen@student.tdsb.on.ca>
2024-05-13 09:50:59 -0400

    comments added for points system

--- src/Column_3.java
+++ src/Column_3.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 
 //This is the entire program
 public class Column_3 {
+    //creat variables to store points
     public int points1 = 0;
     public int points2 = 0;
     public int points3 = 0;
@@ -15,6 +16,7 @@ public class Column_3 {
             String question_1 = JOptionPane.showInputDialog("What is the scientific name of the human");
             if (question_1.equalsIgnoreCase("homosapien") || question_1.equalsIgnoreCase("homo sapien")) {
                 JOptionPane.showMessageDialog(null, "You are correct");
+                //add points when correct awnser
                 points1 = 1;
             } 
             else {
@@ -25,6 +27,7 @@ public class Column_3 {
             String question_2 = JOptionPane.showInputDialog("What human body part stays the same size from birth");
             if (question_2.equalsIgnoreCase("eye") || question_2.equalsIgnoreCase("eyes")) {
                 JOptionPane.showMessageDialog(null, "You are correct");
+                //add points when correct awnser
                 points2 = 2;
             } 
             else {
@@ -35,6 +38,7 @@ public class Column_3 {
             String question_3 = JOptionPane.showInputDialog("Triskaidekaphobia is the fear of this number");
             if (question_3.equalsIgnoreCase("13")) {
                 JOptionPane.showMessageDialog(null, "You are correct");
+                //add points when correct awnser
                 points3 = 3;
             } 
             else {
