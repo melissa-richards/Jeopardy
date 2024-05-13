b2f360e7993faf5bf9eea052901709a5183ff7d1
Zachary <zachary.zheng@student.tdsb.on.ca>
2024-05-13 09:40:18 -0400

    comments

--- src/App.java
+++ src/App.java
@@ -1,10 +1,15 @@
+//import
 import javax.swing.JOptionPane;
 public class App {
+    //main class
     public static void main(String[] args) throws Exception {
+        //askes user if they want to play jeoprdy and if they do run the code else exit the code
         int play = JOptionPane.showConfirmDialog(null, "Do you want to play JEOPRDY",
             "True Or False", JOptionPane.YES_NO_OPTION);
         if (play == JOptionPane.YES_OPTION){
+            // while they keep wanting to play keep on running the code
             while (play == JOptionPane.YES_OPTION) {
+                //ask them what coloumn they want to choose for quesitons and direct them to each class depending on thier answers
                 String Column = (JOptionPane.showInputDialog("Pick a Column 1, 2, or 3"));
                 if ((Column.equals("1"))||(Column.equals("one"))){
                     Column_1 O1 = new Column_1();
@@ -18,6 +23,7 @@ public class App {
                     Column_3 O3 = new Column_3();
                     O3.C3();                
                 }
+                //if it is an error tell the user and rerun the code
                 else{
                     JOptionPane.showMessageDialog(null, "ERROR");
                 }
--- src/Column_1.java
+++ src/Column_1.java
@@ -6,9 +6,13 @@ import javax.swing.JOptionPane;
 public class Column_1 {
     public void C1() throws Exception {
 
+        //set varible exit to false
         boolean exit = false;
+        //while exit is false keep on running the code till its true
         while (exit == false) {
+            //gets user to pick a question 1 2 or 3
             String choice = (JOptionPane.showInputDialog("Pick a quesiton 1, 2, or 3"));
+            //redirect user to each question depending on thier choice and once they answer tell them if correct or incorrect then set exit to true
             if ((choice.equals("1")) || (choice.equals("one"))) {
                 String question_1 = JOptionPane.showInputDialog("Name a police officers favourite snack");
                 if (question_1.equalsIgnoreCase("donuts") || question_1.equalsIgnoreCase("donut")) {
@@ -42,6 +46,7 @@ public class Column_1 {
                     exit = true;
                 }
             } 
+            //if error tell user it is an error and rerun the code
             else {
                 JOptionPane.showMessageDialog(null, "ERROR");
                 exit = false;
